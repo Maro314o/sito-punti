f8b49cb4ae8127ac699d5e76132d53f3bd02e045
maxibon776 <Marottomassimiliano@gmail.com>
2023-10-24 22:53:47 +0200

    Pagine registra e accedi
    aggiunta delle pagine html per creare un account o accederci.Creati anche alcune condizioni per la creazione di un account e metodi per la recupera delle credenziali.Non presente ancora la parte di database.

--- sito/autenticazione.py
+++ sito/autenticazione.py
@@ -1,12 +1,13 @@
-from flask import Blueprint,render_template,request
+from flask import Blueprint,render_template,request,flash
 
 autenticazione=Blueprint('autenticazione',__name__)
 
 @autenticazione.route('/login',methods=['GET','POST'])
 def login():
-    email=request.form.get('email')
-    password=request.form.get('password')
-    print(email,password)
+    if request.method == 'POST':
+        email=request.form.get('email')
+        password=request.form.get('password')
+        print(email,password)
 
     return  render_template("login.html")
 
@@ -17,13 +18,24 @@ def logout():
 @autenticazione.route("/sign_up",methods=['GET','POST'])
 def sign_up():
     if request.method=='POST':
-        email=request.form.get('email')
+        email=(request.form.get('email')).lower()
         firstName=request.form.get('firstName')
         cognome=request.form.get('cognome')
-        password1=request.form.get('password1')
-        password2=request.form.get('password2')
-        if email.lower()==f's-{cognome}.{firstName}@isiskeynes.it':
-            print('balls')
+        password=request.form.get('password')
+        password_di_conferma=request.form.get('password_di_conferma')
+        if '@isiskeynes.it' not in email:
+           flash('Il dominio dell\' email é sbagliato',category='error')
+        if 's-' not in email:
+            flash('hai dimenticato di mettere \'s-\' nell\' email',category='error')
+        if email!=f's-{cognome.lower()}.{firstName.lower()}@isiskeynes.it':
+            flash('L\'email non corrisponde con il nome e cognome',category='error')
+        if len(password)< 5 :
+            flash('La password deve essere almeno di 5 caratteri',category='error')
+        if password != password_di_conferma:
+            flash('La password di conferma non e\' corretta',category='error')
+        flash('Account creato con successo!',category='success')
+
+
 
 
     return render_template("sign_up.html")
