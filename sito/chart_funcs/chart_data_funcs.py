069316f595f0519f25f3a6da8ef5f421a0e871fd
Massimiliano Marotto <marottomassimiliano@gmail.com>
2024-09-27 23:25:42 +0200

    refactor e type annotation di funzioni sulla cronologia

--- sito/chart_funcs/chart_data_funcs.py
+++ sito/chart_funcs/chart_data_funcs.py
@@ -1,9 +1,9 @@
 from sito.modelli import Cronologia
 
 
-def list_data(eventi: list[Cronologia], stagione: int) -> list[int]:
+def elenco_punti_cumulativi(eventi: list[Cronologia], stagione: int) -> list[int]:
     return [evento.punti_cumulativi for evento in eventi if evento.stagione == stagione]
 
 
-def list_label(eventi: list[Cronologia], stagione: int) -> list[str]:
+def elenco_date(eventi: list[Cronologia], stagione: int) -> list[str]:
     return [evento.data for evento in eventi if evento.stagione == stagione]
