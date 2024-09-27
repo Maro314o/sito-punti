0f3b1e1018f8e8164813eea0c4d8ac671dd8fb42
Massimiliano Marotto <marottomassimiliano@gmail.com>
2024-09-27 23:46:17 +0200

    refactor e type annotation di funzioni database queries

--- sito/database_funcs/database_queries.py
+++ sito/database_funcs/database_queries.py
@@ -1,34 +1,34 @@
 from ..modelli import User, Classi, Cronologia
 
 
-def cronologia_da_user(utente):
-    return utente.cronologia_studente
-
-
-def user_da_nominativo(nominativo):
+def user_da_nominativo(nominativo: str) -> User | None:
     return User.query.filter_by(nominativo=nominativo).first()
 
 
-def user_da_id(id):
+def user_da_id(id: int) -> User | None:
     return User.query.filter_by(id=id).first()
 
 
-def user_da_email(email):
+def user_da_email(email: str) -> User | None:
     return User.query.filter_by(email=email).first()
 
 
-def classe_da_nome(classe_name):
-    return Classi.query.filter_by(classe=classe_name).first()
-
-
-def classe_da_id(classe_id):
-    return Classi.query.filter_by(id=classe_id).first()
-
-
-def studenti_da_classe(classe):
+def studenti_da_classe(classe: Classi) -> list[User]:
 
     return classe.studenti
 
 
-def evento_da_id(id):
+def classe_da_nome(classe_name: str) -> Classi | None:
+    return Classi.query.filter_by(classe=classe_name).first()
+
+
+def classe_da_id(classe_id: int) -> Classi | None:
+    return Classi.query.filter_by(id=classe_id).first()
+
+
+def cronologia_da_user(utente: User) -> list[Cronologia]:
+    return utente.cronologia_studente
+
+
+def evento_da_id(id: int) -> Cronologia | None:
     return Cronologia.query.filter_by(id=id).first()
