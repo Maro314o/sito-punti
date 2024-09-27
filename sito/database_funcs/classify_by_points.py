abd592f88c7bbc4b335e413c4b729071ae48c2b1
Massimiliano Marotto <marottomassimiliano@gmail.com>
2024-09-27 23:31:14 +0200

    refactor e type annotation di funzioni di classifica

--- sito/database_funcs/classify_by_points.py
+++ sito/database_funcs/classify_by_points.py
@@ -1,20 +1,20 @@
+from ..modelli import User, Classe
 from .database_queries import studenti_da_classe
-
 from .. import app
 
 with app.app_context():
     from .list_database_elements import elenco_studenti
 
-    def classifica_user(stagione, studenti):
+    def classifica_user(stagione: int, users: list[User]) -> list[User]:
         return sorted(
-            studenti,
-            key=lambda studente: float(studente.punti.split(",")[stagione - 1]),
+            users,
+            key=lambda user: float(user.punti.split(",")[stagione - 1]),
         )[::-1]
 
-    def classifica_studenti_di_una_classe(stagione, classe):
+    def classifica_studenti_di_una_classe(stagione: int, classe: Classe) -> list[User]:
         studenti = studenti_da_classe(classe)
         return classifica_user(stagione, studenti)
 
-    def classifica_studenti(stagione):
+    def classifica_studenti(stagione: int) -> list[User]:
         studenti = elenco_studenti()
         return classifica_user(stagione, studenti)
