cc9cbbd8a4f55c69ef53dc372d383177876b158e
Massimiliano Marotto <marottomassimiliano@gmail.com>
2024-09-27 23:51:36 +0200

    refactor e type annotation delle funzioni di elenco delle tabelle del database

--- sito/database_funcs/list_database_elements.py
+++ sito/database_funcs/list_database_elements.py
@@ -3,43 +3,41 @@ from ..modelli import User, Classi, Info
 from .database_queries import studenti_da_classe
 
 
-def elenco_utenti():
+def elenco_utenti() -> list[User]:
     return User.query.filter_by().all()
 
 
-def elenco_studenti():
+def elenco_studenti() -> list[User]:
 
     return [utente for utente in User.query.filter_by().all() if not utente.admin_user]
 
 
-def elenco_admin():
+def elenco_admin() -> list[User]:
     return [utente for utente in User.query.filter_by().all() if utente.admin_user]
 
 
-def elenco_tutte_le_classi():
-    return Classi.query.filter_by().all()
+def elenco_studenti_registrati() -> list[User]:
+    return [studente for studente in elenco_studenti() if studente.account_attivo]
 
 
-def elenco_classi_studenti():
-    return [classe for classe in elenco_tutte_le_classi() if classe.classe != "admin"]
-
-
-def elenco_squadre_da_classe(classe):
-    return set([x.squadra for x in studenti_da_classe(classe)])
-
-
-def elenco_studenti_registrati():
-    return [x for x in elenco_studenti() if x.account_attivo]
-
-
-def elenco_user_da_classe_id_e_nome_squadra(classe_id, nome_squadra):
+def elenco_user_da_classe_id_e_nome_squadra(
+    classe_id: int, nome_squadra: str
+) -> list[User]:
 
     return User.query.filter_by(classe_id=classe_id, squadra=nome_squadra).all()
 
 
-def elenco_squadre_da_classe(classe):
-    return {x.squadra for x in studenti_da_classe(classe)}
+def elenco_tutte_le_classi() -> list[Classi]:
+    return Classi.query.filter_by().all()
 
 
-def get_last_season():
+def elenco_classi_studenti() -> list[Classi]:
+    return [classe for classe in elenco_tutte_le_classi() if classe.classe != "admin"]
+
+
+def elenco_squadre_da_classe(classe: Classi) -> set[str]:
+    return {studente.squadra for studente in studenti_da_classe(classe)}
+
+
+def get_last_season() -> int:
     return Info.query.filter_by().first().last_season
