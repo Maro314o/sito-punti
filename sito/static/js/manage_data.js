05c31ef9669c2687485cc148b28d4a804b0e739b
pastasugo89 <pastasugo89@gmail.com>
2025-11-23 11:47:48 +0100

    added selectAll checkbox for column 'telefono' and 'presenza'

--- sito/static/js/manage_data.js
+++ sito/static/js/manage_data.js
@@ -1,26 +1,29 @@
 document.getElementById("classSelector").addEventListener("change", () => {
-            document.getElementById("classForm").submit();
-        });
-        document.getElementById("dateSelector").addEventListener("change", () => {
-            document.getElementById("classForm").submit();
-        });
-        document.getElementById('customFileInput').addEventListener('change', function (event) {
-            var fileInput = event.target;
-            var fileName = fileInput.files.length ? fileInput.files[0].name : '';
-
-            if (fileName) {
-                // Mostra l'icona e il nome del file selezionato
-                document.getElementById('upload-icon').style.display = 'flex';
-                document.getElementById('file-name').textContent = fileName;
-            } else {
-                // Nasconde l'icona se non c'è nessun file selezionato
-                document.getElementById('upload-icon').style.display = 'none';
-            }
-        });
-
-const selectAllCheckbox = document.getElementById('selectAll');
-const alunniCheckboxes = document.querySelectorAll('.alunno');
-
-selectAllCheckbox.addEventListener('change', () => {
-    alunniCheckboxes.forEach(cb => cb.checked = selectAllCheckbox.checked);
+    document.getElementById("classForm").submit();
 });
+document.getElementById("dateSelector").addEventListener("change", () => {
+    document.getElementById("classForm").submit();
+});
+
+
+document.getElementById("selectAllTelefono").addEventListener("change", function () {
+    const allPlus = document.querySelectorAll(".plus");
+    const allMinus = document.querySelectorAll(".minus");
+
+    if (this.checked) {
+        allPlus.forEach(cb => cb.checked = true);
+        allMinus.forEach(cb => cb.checked = false);
+    } else {
+        allPlus.forEach(cb => cb.checked = false);
+        allMinus.forEach(cb => cb.checked = false);
+    }
+});
+
+document.querySelector('.selectAllPresenza').addEventListener('change', function () {
+    const allSelects = document.querySelectorAll('.stato');
+
+    allSelects.forEach(sel => {
+        sel.value = this.checked ? 'presente' : 'assente';
+    });
+});
+
