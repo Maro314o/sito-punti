86f899afaf381e36c9fedd6c096f714188fd08c5
Massimiliano Marotto <massimilianomarotto@proton.me>
2025-12-19 18:59:17 +0100

    ennesima rimozione dei file di setup

--- setup.bat
+++
@@ -1,3 +0,0 @@
-pip3 install --upgrade pip
-pip3 install -r requirements.txt
-pip3 install  Werkzeug==2.2.2
--- setup.sh
+++
@@ -1,4 +0,0 @@
-pip3 install --upgrade pip
-pip3 install -r requirements.txt
-pip3 install  Werkzeug==2.2.2
-#per un bug di flask 3.0.0 c'e' bisogno della versione 2.2.2 di Werkzeug,altrimenti non puo' funzionare
